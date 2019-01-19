313a9f14d8de91faab46d7d147f223bef7f77349
lb308 <lahcenelbouchti@gmail.com>
2019-01-19 20:51:26 +0100

    Use JWT Middleware in controllers

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -8,6 +8,15 @@ use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('JWT');
+    }
 
     public function like(Reply $reply)
     {
